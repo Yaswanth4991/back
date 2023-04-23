0fb3689c8e35ccbc004dfbaf4409aded37d980ef
Yaswanth4991 <you@example.comyaswanthyash7726@gmail.com>
2023-04-23 15:55:20 +0530

    bakend

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const multer = require('multer');
 const cors = require('cors')
 
 
-mongoose.connect('mongodb+srv://yaswanthyash7726:Yash@123@cluster0.1mbtocr.mongodb.net/test', { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect('mongodb+srv://yaswanthyash7726:yaswanthyash7726@cluster0.1mbtocr.mongodb.net/?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('Connected to MongoDB...'))
   .catch(err => console.error('Could not connect to MongoDB...', err));
 
